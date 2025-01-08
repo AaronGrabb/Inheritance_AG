c4c8a056cbc4260517a82daeeeceb499e593d0ef
AaronGrabb <aarongrabb13@gmail.com>
2025-01-08 14:07:08 -0700

    add

---
+++ src/TwoDArrays/Ex1_GolfCard.java
@@ -0,0 +1,20 @@
+package TwoDArrays;
+
+public class Ex1_GolfCard {
+
+public static void run(){
+    String[] names = { "Bob", "Sabrina", " Gill", "Steve"};
+    int[][] scoreCard = new int[4][18];
+
+    System.out.println(scoreCard.length);
+
+
+
+    for (int row = 0; row < scoreCard.length; row++){
+
+    }
+
+
+    }
+
+}
