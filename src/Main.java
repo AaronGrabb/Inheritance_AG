aa387cc879f94f83fdd55fe860b9283a240cc0d3
AaronGrabb <aarongrabb13@gmail.com>
2025-03-12 15:25:35 -0600

    add

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import Ex1_Animals.Ex1_Main;
 //import Ex2_Restaurant.Ex2_Main;
 import Ex3_MusicFestival.Ex3_Main;
+import TwoDArrays.As5_Vigenere;
 import TwoDArrays.Ex1_GolfCard;
 import TwoDArrays.Ex2_TicTacToe;
 import TwoDArrays.As1_Bingo;
+import FamilyTree.FamilyTreeMain;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +17,10 @@ public class Main {
 //        Ex3_Main.run();
 //        Ex1_GolfCard.run();
 //        Ex2_TicTacToe.run();
-        As1_Bingo.run();
+//        As1_Bingo.run();
+//        As5_Vigenere.run();
+        FamilyTreeMain.run();
+
     }
 
 }
--- src/TwoDArrays/As1_Bingo.java
+++ src/TwoDArrays/As1_Bingo.java
@@ -1,17 +1,59 @@
 package TwoDArrays;
-
+import java.util.*;
 public class As1_Bingo {
-    char[] bingo = {'B','I','N','G','O'};
+
 
     public static void run(){
+        char[] bingo = {'B','I','N','G','O'};
         int[][] bingoSheet = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            System.out.print(bingo[i]+ ":  | ");
+            bingoSheet[i] = generateArray(5,1+i*15,15+i*15);
+            for (int j = 0; j < 5; j++) {
+                System.out.print(bingoSheet[i][j] + " | ");
+            }
+            System.out.println();
+        }
+while(true){
+    int random = -1;
+
+    String generate = MyStuff.Library.input.nextLine();
+    if(generate.equals("")){
+        random = myRandom(1,75);
+        System.out.println("New Roll: "+random);
+boolean around = false;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(random == bingoSheet[i][j]){
+                    bingoSheet[i][j] = 0;
+                    System.out.println("Number found is " + random);
+                    around = true;
+                }
+            }
+        }//Check match
+        if(around == false){
+            System.out.println("Number not found.");
+        }
 
         for (int i = 0; i < 5; i++) {
+            System.out.print(bingo[i]+ ":  | ");
+            for (int j = 0; j < 5; j++) {
+                System.out.print(bingoSheet[i][j] + " | ");
+            }
+            System.out.println();
+        }//Draw new grid
 
-                bingoSheet[i] = generateArray(5,1+i*15,15+i*15);
+    }
 
-
+    for (int i = 0; i < 5; i++) {
+        if(    checkRow(bingoSheet[i]) || checkCol(bingoSheet)){
+            System.out.println("You Won!");
+            break;
         }
+    }
+
+}
+
 
 
     }
@@ -40,5 +82,37 @@ return array;
         int num =  (int) (Math.random() * (max - min + 1)   + min);
         return num;
     }//and random
+
+    public static boolean checkRow(int[] row){
+        int numZero = 0;
+        for (int i = 0; i < row.length; i++) {
+            if(row[i] == 0){
+                numZero++;
+            }
+        }
+        if(numZero == 5){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean checkCol(int[][] col){
+        for (int i = 0; i < col.length; i++) {
+            int numZero = 0;
+            for (int j = 0; j < col.length; j++) {
+                if(col[j][i] == 0){
+                    numZero++;
+                }
+            }
+            if(numZero == 5){
+                return true;
+            }
+
+        }
+return false;
+    }
+
 
 }
